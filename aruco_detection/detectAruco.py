c063f4aca16b661ae34de2bfa74ebf847aca20b6
Michal Trela <mmichal.trela@gmail.com>
2023-05-29 01:04:08 +0200

    calculate distance between markers, mark middle

--- aruco_detection/detectAruco.py
+++ aruco_detection/detectAruco.py
@@ -5,6 +5,7 @@ import json
 import numpy as np
 from datetime import datetime
 
+
 def detectAndMarkAruco(frame, arucoDetector):
     markerCorners, markerIds, rejectedCandidates = arucoDetector.detectMarkers(frame)
 
@@ -30,35 +31,60 @@ def detectAndMarkAruco(frame, arucoDetector):
 
 def poseEstimationAruco(frame, matrixCoeff, distortionCoeff, arucoDetector):
 
+    # kernel = np.ones((5,5),np.float32)/25
+    # frame = cv.filter2D(frame,-1,kernel)
+    frame = cv.bilateralFilter(frame,9,100,100)
+
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     markerCorners, markerIDs, rejectedCandidates = arucoDetector.detectMarkers(gray)
-    markerSizeInM = 0.099
+    markerSizeInM = 0.057
 
     if len(markerCorners) > 0:
-        for i in range(0, len(markerIDs)):
+        # for i in range(0, len(markerIDs)):
 
-            rvec, tvec, markerPoints = cv.aruco.estimatePoseSingleMarkers(markerCorners[i], markerSizeInM, matrixCoeff, distortionCoeff)
-            cv.drawFrameAxes(frame, matrixCoeff, distortionCoeff, rvec, tvec, markerSizeInM)
+            # rvec, tvec, markerPoints = cv.aruco.estimatePoseSingleMarkers(markerCorners[i], markerSizeInM, matrixCoeff, distortionCoeff)
+            # cv.drawFrameAxes(frame, matrixCoeff, distortionCoeff, rvec, tvec, markerSizeInM)
 
-        markerIDs = markerIDs.flatten()
         # cv.aruco.drawDetectedMarkers(frame, markerCorners, markerIDs)
 
+        
+        markerIDs = markerIDs.flatten()
+        # print(markerCorners)
+        
+        rvec, tvec, _ = cv.aruco.estimatePoseSingleMarkers(markerCorners, markerSizeInM, matrixCoeff, distortionCoeff)
 
-        for (markerCorner, markerID) in zip(markerCorners, markerIDs):
+        markersCenter = []
+
+        for (markerCorner, markerID, i) in zip(markerCorners, markerIDs, range(0, markerIDs.size)):
+            cv.drawFrameAxes(frame, matrixCoeff, distortionCoeff, rvec[i], tvec[i], markerSizeInM)
+            # print(tvec[i])
             (topLeft, topRight, bottomRight, bottomLeft) = markerCorner.reshape((4, 2))
             topRight = (int(topRight[0]), int(topRight[1]))
             bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
+            # print(topRight)
             cv.line(frame, topLeft, topRight, (0, 255, 0), 2)
             cv.line(frame, topRight, bottomRight, (0, 255, 0), 2)
             cv.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
             cv.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
-            distance = np.sqrt(
-                tvec[0][0][2] ** 2 + tvec[0][0][0] ** 2 + tvec[0][0][1] ** 2
-            )
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            markersCenter.append((cX, cY))
+            distance = np.sqrt(tvec[i][0][2] ** 2 + tvec[i][0][0] ** 2 + tvec[i][0][1] ** 2)
             cv.putText(frame, f'{distance:.2f}m', (topLeft[0], topLeft[1] - 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             
+        # print(markersCenter[0][0])
+        if len(markerCorners) > 1:
+            for i in range(0, len(markerIDs) - 1):
+                cv.line(frame, markersCenter[i], markersCenter[i+1], (0, 255, 0), 2)
+                distance = np.linalg.norm(tvec[i]-tvec[i+1])
+                middle = (int((markersCenter[i][0] + markersCenter[i + 1][0]) /2), int((markersCenter[i][1] + markersCenter[i + 1][1]) /2))
+                # print(middle)
+
+                cv.circle(frame, middle, 4, (0, 0, 255), -1)
+                cv.putText(frame, f'{distance:.2f}m', (middle[0] + 5, middle[1] - 5), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        
     return frame
 
 
